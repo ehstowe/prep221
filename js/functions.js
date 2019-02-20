a079c82e569987c9b2b80f1e5a4d3958e4da797e
ehstowe <47224648+ehstowe@users.noreply.github.com>
2019-02-20 13:28:37 -0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -54,8 +54,9 @@ var calculateMode=function()
 
   
 
-var reduceFunction=function(data)
+var reduceFunction=function()
 {
+var data = [11, 4, 41, 50, 7, 19, 8, 18, 2, 6, 43, 6, 20, 43, 2, 23, 12, 24, 31, 29];
 var buttonReduce=document.getElementById("Reduce Button");
   buttonReduce.remove();
 var sum = data.reduce(function(acc, element)
